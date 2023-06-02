1d3b59071107c3ab966ef9ec17c3d852b5277cc2
39rep <okome3317@gmail.com>
2023-06-02 21:32:06 +0900

    createPost.js & Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,23 @@
 import React from "react";
+import "./style/Home.css";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="homePage">
+      <div className="postContents">
+        <div className="postHeader">
+          <h1>タイトル</h1>
+        </div>
+        <div className="postTextContainer">
+          今はReactの学習中です。頑張るぞい。頑張るぞい。頑張るぞい。頑張るぞい。頑張るぞい。頑張るぞい。頑張るぞい。頑張るぞい。頑張るぞい。
+        </div>
+        <div className="nameAndDeleteButton">
+          <h3>@replica</h3>
+          <button>削除</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
