f50fe4ee5f712f1aa6e14fa0d08d9544ed9185b5
39rep <okome3317@gmail.com>
2023-06-02 17:19:07 +0900

    setup Auth

---
+++ src/components/Navber.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./style/Navbar.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faHouse,
+  faFilePen,
+  faArrowRightToBracket,
+  faArrowRightFromBracket,
+} from "@fortawesome/free-solid-svg-icons";
+
+const Navber = ({ isAuth }) => {
+  return (
+    <nav>
+      <Link to="/">
+        <FontAwesomeIcon icon={faHouse} />
+        ホーム
+      </Link>
+      <Link to="/createpost">
+        <FontAwesomeIcon icon={faFilePen} />
+        記事投稿
+      </Link>
+      {!isAuth ? (
+        <Link to="/login">
+          <FontAwesomeIcon icon={faArrowRightToBracket} />
+          ログイン
+        </Link>
+      ) : (
+        <Link to="/logout">
+          <FontAwesomeIcon icon={faArrowRightFromBracket} />
+          ログアウト
+        </Link>
+      )}
+    </nav>
+  );
+};
+
+export default Navber;
